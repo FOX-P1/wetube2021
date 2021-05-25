7cf531fb6ed7da8a4a96f1cedc96cbda071b86c7
FOX-P1 <smgms7513@gmail.com>
2021-05-25 09:19:51 +0900

    wetube_3

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,19 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  Video.find({}, (error, videos) => {
-    console.log("errors", error);
-    console.log("videos", videos);
-  });
-  console.log("hello");
-  return res.render("home", { pageTitle: "Home", videos: [] });
+/*
+Video.find({}, (error, videos) => {
+    if(error){
+      return res.render("server-error")
+    }
+    return res.render("home", { pageTitle: "Home", videos });
+});
+
+*/
+
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  console.log(videos);
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
@@ -26,7 +33,22 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
   
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      createdAt: "lalalalalal",
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      meta: {
+        views: 0,
+        rating: 0,
+      },
+    });
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    return res.render("upload", { pageTitle: "Upload Video" });
+  }
 };
