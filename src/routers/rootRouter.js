23ec934c4e3d025ff6ba26f1685e09ce96a6319c
FOX-P1 <smgms7513@gmail.com>
2021-06-24 19:31:43 +0900

    wetube_recap_2

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -4,7 +4,6 @@ import { home, search } from "../controllers/videoController";
 import { publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
-
 rootRouter.get("/", home);
 rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
 rootRouter.route("/login").all(publicOnlyMiddleware).get(getLogin).post(postLogin);
